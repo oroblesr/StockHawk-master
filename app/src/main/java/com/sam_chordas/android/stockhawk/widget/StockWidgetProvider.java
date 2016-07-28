1e1cb8483f62aa64136f9daaa802ec5015ee04f1
orobles <oscar.roblesr@gmail.com>
2016-07-27 21:28:56 -0500

    Added basic functionality to the widget

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
@@ -1,11 +1,64 @@
 package com.sam_chordas.android.stockhawk.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
 
 /**
  * Created by oroblesr on 7/20/16.
  */
 public class StockWidgetProvider extends AppWidgetProvider {
 
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
 
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_stock);
+
+            // Create an Intent to launch MyStocksActivity
+            //TODO Change to the right activity
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+           // remoteViews.setOnClickPendingIntent(R.id.widget_list, pendingIntent);
+
+            remoteViews.setRemoteAdapter(R.id.widget_list,
+                    new Intent(context, StockWidgetRemoteViewsService.class));
+
+            Intent clickIntentTemplate = new Intent(context, MyStocksActivity.class);
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setPendingIntentTemplate(R.id.widget_list, clickPendingIntentTemplate);
+            // TODO setEmptyView
+           // remoteViews.setEmptyView(R.id.widget_list, R.id.widget_empty);
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                new ComponentName(context, getClass()));
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+
+    }
 }
--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetRemoteViewsService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetRemoteViewsService.java
@@ -1,16 +1,110 @@
 package com.sam_chordas.android.stockhawk.widget;
 
-import android.app.IntentService;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 
 /**
  * Created by oroblesr on 7/20/16.
  */
 public class StockWidgetRemoteViewsService extends RemoteViewsService {
 
+    // These indices are taken from //TODO
+    static final int STOCK_ID = 0;
+
+
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new RemoteViewsFactory() {
+            private final int NUMBER_OF_VIEWS = 1;
+            private Cursor cursor;
+            //The same id always refers to the same object.
+            private final boolean HAS_STABLE_IDS = true;
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (cursor != null){
+                    cursor.close();
+                }
+
+                final long identityToken = Binder.clearCallingIdentity();
+
+                try {
+                    cursor = getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            null);
+
+                }
+                finally {
+
+                    Binder.restoreCallingIdentity(identityToken);
+
+                }
+
+            }
+
+            @Override
+            public void onDestroy() {
+                if (cursor != null) {
+                    cursor.close();
+                    cursor = null;
+                }
+
+            }
+
+            @Override
+            public int getCount() {
+                return cursor!= null ? cursor.getCount() : 0;
+            }
+
+            @Override
+            public RemoteViews getViewAt(int i) {
+                RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.widget_list_item);
+
+                //TODO Complete
+                remoteViews.setTextViewText(R.id.widget_symbol, "Text from service");
+
+                final Intent fillInIntent = new Intent();
+
+                remoteViews.setOnClickFillInIntent(R.id.widget_frame_list_item, fillInIntent);
+
+                return remoteViews;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_stock);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return NUMBER_OF_VIEWS;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                if (cursor.moveToPosition(i))
+                    return cursor.getLong(STOCK_ID);
+                return i;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return HAS_STABLE_IDS;
+            }
+        };
     }
 }
